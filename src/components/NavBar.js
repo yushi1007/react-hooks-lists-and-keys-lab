6dc04c8c7ba027cb0a6cd3fb2a404b7a3f580c85
Yu Shi <yushiys95@gmail.com>
2021-04-05 22:50:55 -0400

    commit tree 378f2f5b3fbb23be4b5fd74ebb39c87a0d157b35

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+// can I use different variable name here?
+  const anchors = links.map((link) => (
+    <a key={link} href={"#" + link}>
+      {link}
+    </a>
+  ));
+  return <nav>{anchors}</nav>;
 }
 
 export default NavBar;
